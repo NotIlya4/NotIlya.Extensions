745624bd018a650474d3f6582b8e63270a70229c
NotIlya <secretman44444@gmail.com>
2023-05-27 11:23:45 +0300

    Add strings extensions

---
+++ src/NotIlya.Extensions.Tests/StringExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿namespace NotIlya.Extensions.Tests;
+
+public class StringExtensionsTests
+{
+    private readonly List<string> _strings = new List<string>() {"Biba", "Boba", "Aaa", "asd"};
+    
+    [Fact]
+    public void JoinStrings_StringsList_ConcatedStringsWithSeparator()
+    {
+        string expect = "Biba, Boba, Aaa, asd";
+
+        string result = _strings.JoinStrings();
+        
+        Assert.Equal(expect, result);
+    }
+    
+    [Fact]
+    public void JoinStrings_StringsListWithSpecifiedSeparator_ConcatedStringsWithNewSeparator()
+    {
+        string expect = "Biba||Boba||Aaa||asd";
+
+        string result = _strings.JoinStrings("||");
+        
+        Assert.Equal(expect, result);
+    }
+}
---
+++ src/NotIlya.Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace NotIlya.Extensions;
+
+public static class StringExtensions
+{
+    public static string JoinStrings(this IEnumerable<string> strings, string separator = ", ")
+    {
+        return string.Join(separator, strings);
+    }
+}
